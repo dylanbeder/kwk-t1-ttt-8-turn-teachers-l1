43d9aeaa158ee77307d9bf60fa1dc708dc529859
dylanbeder <dylan.beder@duke.edu>
2018-06-09 23:46:15 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,11 @@
 def turn(board)
   puts "Please enter 1-9:"
-  index=.gets
+  ind=gets.strip
+  index=input_to_index(ind)
   if valid_move?(board,index)
     move(board,index,character)
+  else
+    puts "Please enter 1-9:"
   end
 end
 
